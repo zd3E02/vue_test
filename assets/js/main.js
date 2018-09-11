eb8dcd22088b7ab00e21db3a61cc9e6371b75459
zd3E02 <zd.imamura@gmail.com>
2018-09-11 16:10:51 +0900

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
 new Vue({
-  el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 }
-    ]
-  }
+  el: '#app'
 })
